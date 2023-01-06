fccdeca33dac1e542976bab938267f8e4e172892
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-06 23:07:32 +0900

    removed dummy list

--- src/App.js
+++ src/App.js
@@ -16,21 +16,6 @@ function App() {
       });
   }
 
-  const dummyMovies = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18",
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-  ];
-
   return (
     <React.Fragment>
       <section>
